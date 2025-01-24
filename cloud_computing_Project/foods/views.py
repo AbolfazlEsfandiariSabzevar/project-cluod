1e080fe72692657ea1ccdd0a7e96046c67b66703
AbolfazlEsfandiari <abolfazlesfandiari103@gmail.com>
2025-01-24 15:08:00 +0330

    Update views.py

--- cloud_computing_Project/foods/views.py
+++ cloud_computing_Project/foods/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
-from .models import Food  # تغییر به مدل غذا
-from .serializers import FoodSerializer  # تغییر به سریالایزر غذا
+from .models import Food  
+from .serializers import FoodSerializer 
 
 class FoodViewSet(viewsets.ModelViewSet):
-    queryset = Food.objects.all()  # تغییر به queryset مربوط به غذا
-    serializer_class = FoodSerializer  # استفاده از سریالایزر جدید غذا
+    queryset = Food.objects.all()  
+    serializer_class = FoodSerializer 
